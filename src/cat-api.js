76eb72b11b64a05350d9f4ac410214fcd7aaf3ad
Kushnir88 <mr.kushnir.igor@gmail.com>
2023-05-27 23:42:06 +0300

    update HW10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,24 +1,33 @@
-// Функція для виконання HTTP-запиту та повернення промісу з результатом
-function fetchData(url) {
-    return fetch(url, {
-      headers: {
-        'x-api-key': 'live_7T1AZid9vL75EqfurdhsQyF5aHlQssLaw3xr3sYdppae7KSAJSDJzlDoivV53c65'
-      }
-    })
-      .then(response => response.json())
-      .catch(error => {
-        throw new Error('Сталася помилка під час виконання запиту');
-      });
-  }
-  
-  // Функція для отримання списку порід
-  export function fetchBreeds() {
-    const url = 'https://api.thecatapi.com/v1/breeds';
-    return fetchData(url);
-  }
-  
-  // Функція для отримання інформації про кота за породою
-  export function fetchCatByBreed(breedId) {
-    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-    return fetchData(url);
-  }
+const API_KEY = 'live_7T1AZid9vL75EqfurdhsQyF5aHlQssLaw3xr3sYdppae7KSAJSDJzlDoivV53c65';
+
+export function fetchBreeds() {
+  const url = 'https://api.thecatapi.com/v1/breeds';
+
+  return fetch(url, {
+    headers: {
+      'x-api-key': API_KEY
+    }
+  })
+    .then(response => response.json())
+    .then(data => data.map(breed => ({ id: breed.id, name: breed.name })))
+    .catch(error => {
+      console.error('Error fetching breeds:', error);
+      throw error;
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+
+  return fetch(url, {
+    headers: {
+      'x-api-key': API_KEY
+    }
+  })
+    .then(response => response.json())
+    .then(data => data[0])
+    .catch(error => {
+      console.error('Error fetching cat:', error);
+      throw error;
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,107 +1,56 @@
-  // app.js
-
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-const breedSelect = document.querySelector('.breed-select');
+const breedSelect = document.getElementById('breed-select');
 const catInfo = document.querySelector('.cat-info');
+const catImage = document.getElementById('cat-image');
+const catBreed = document.getElementById('cat-breed');
+const catDescription = document.getElementById('cat-description');
+const catTemperament = document.getElementById('cat-temperament');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 
-// Функція для заповнення селекту зі списком порід
-function populateBreedSelect(breeds) {
-  breeds.forEach(breed => {
-    const option = document.createElement('option');
-    option.value = breed.id;
-    option.text = breed.name;
-    breedSelect.appendChild(option);
-  });
+function populateBreeds() {
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+
+      breedSelect.addEventListener('change', handleBreedSelect);
+      breedSelect.removeAttribute('disabled');
+      loader.style.display = 'none';
+    })
+    .catch(() => {
+      showError();
+      loader.style.display = 'none';
+    });
 }
 
-// Функція для відображення інформації про кота
-function displayCatInfo(cat) {
-  const catImage = catInfo.querySelector('.cat-image');
-  const breedName = catInfo.querySelector('.breed-name');
-  const breedDescription = catInfo.querySelector('.breed-description');
-  const breedTemperament = catInfo.querySelector('.breed-temperament');
+function handleBreedSelect() {
+  const breedId = breedSelect.value;
 
-  catImage.src = cat.url;
-  breedName.textContent = cat.breeds[0].name;
-  breedDescription.textContent = cat.breeds[0].description;
-  breedTemperament.textContent = cat.breeds[0].temperament;
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      catBreed.textContent = cat.breeds[0].name;
+      catDescription.textContent = cat.breeds[0].description;
+      catTemperament.textContent = `Temperament: ${cat.breeds[0].temperament}`;
+      catImage.src = cat.url;
 
-  catInfo.style.display = 'block';
+      catInfo.style.display = 'block';
+      loader.style.display = 'none';
+    })
+    .catch(() => {
+      showError();
+      loader.style.display = 'none';
+    });
 }
 
-// Функція для очищення інформації про кота
-function clearCatInfo() {
-  const catImage = catInfo.querySelector('.cat-image');
-  const breedName = catInfo.querySelector('.breed-name');
-  const breedDescription = catInfo.querySelector('.breed-description');
-  const breedTemperament = catInfo.querySelector('.breed-temperament');
-
-  catImage.src = '';
-  breedName.textContent = '';
-  breedDescription.textContent = '';
-  breedTemperament.textContent = '';
-
-  catInfo.style.display = 'none';
-}
-
-// Функція для відображення завантажувача
-function showLoader() {
-  loader.style.display = 'block';
-}
-
-// Функція для приховування завантажувача
-function hideLoader() {
-  loader.style.display = 'none';
-}
-
-// Функція для відображення помилки
 function showError() {
   error.style.display = 'block';
 }
 
-// Функція для приховування помилки
-function hideError() {
-  error.style.display = 'none';
-}
-
-// Обробник події при виборі породи
-breedSelect.addEventListener('change', () => {
-  const breedId = breedSelect.value;
-
-  if (breedId) {
-    clearCatInfo();
-    showLoader();
-    hideError();
-
-    fetchCatByBreed(breedId)
-      .then(cat => {
-        hideLoader();
-        displayCatInfo(cat);
-      })
-      .catch(() => {
-        hideLoader();
-        showError();
-      });
-  } else {
-    clearCatInfo();
-  }
-});
-
-// Завантаження списку порід при завантаженні сторінки
-window.addEventListener('DOMContentLoaded', () => {
-  showLoader();
-  hideError();
-
-  fetchBreeds()
-    .then(breeds => {
-      hideLoader();
-      populateBreedSelect(breeds);
-    })
-    .catch(() => {
-      hideLoader();
-      showError();
-    });
-});
+loader.style.display = 'block';
+populateBreeds();
